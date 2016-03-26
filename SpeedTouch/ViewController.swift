025a545e0ca59d63e61ec8c79494a143541e3a38
EviloBivl <paul_neuhold@hotmail.com>
2016-03-26 21:45:39 +0100

    Initial Commit

---
+++ SpeedTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeedTouch
+//
+//  Created by Paul Neuhold on 26.03.16.
+//  Copyright © 2016 Paul Neuhold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
