08c167ef5e8cbf623b4edfb23805c90e6226ac4c
Paul Neuhold <paul.neuhold@lyoness.ag>
2017-02-21 13:25:22 +0100

    Testing branching

--- SpeedTouch/ViewController.swift
+++ SpeedTouch/ViewController.swift
@@ -13,11 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.aFunction()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //nothing from importance
+    }
+    
+    func aFunction(){
+        //do nothing
+    
+    //more unimportant stuff
     }
 
 
